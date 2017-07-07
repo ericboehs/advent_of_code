6e9ad788221b1c30edf600dc1c1178a90c6e587b
Eric Boehs <ericboehs@gmail.com>
2017-07-06 20:10:54 -0500

    Remove unused memoization and parens

--- day_1.rb
+++ day_1.rb
@@ -37,7 +37,7 @@ module DayOne
 
     # @return [Array] Revisited coordinates from 0, 0
     def first_coordinates_revisited
-      previous_coordinates.detect { |coordinates| previous_coordinates.count(coordinates) > 1 }
+      previous_coordinates.find { |coordinates| previous_coordinates.count(coordinates) > 1 }
     end
 
     private
@@ -53,7 +53,7 @@ module DayOne
     end
 
     def travel
-      @travel ||= directions.each do |segment|
+      directions.each do |segment|
         turn segment[:direction]
         step segment[:distance]
       end
@@ -109,14 +109,14 @@ require 'minitest/autorun'
 module DayOne
   class PathTest < MiniTest::Test
     def path
-      @path ||= DayOne::Path.new(%w[
+      DayOne::Path.new %w[
         R1 R3 L2 L5 L2 L1 R3 L4 R2 L2 L4 R2 L1 R1 L2 R3 L1 L4 R2 L5 R3 R4 L1 R2 L1 R3 L4 R5 L4 L5 R5 L3 R2 L3
         L3 R1 R3 L4 R2 R5 L4 R1 L1 L1 R5 L2 R1 L2 R188 L5 L3 R5 R1 L2 L4 R3 R5 L3 R3 R45 L4 R4 R72 R2 R3 L1 R1
         L1 L1 R192 L1 L1 L1 L4 R1 L2 L5 L3 R5 L3 R3 L4 L3 R1 R4 L2 R2 R3 L5 R3 L1 R1 R4 L2 L3 R1 R3 L4 L3 L4
         L2 L2 R1 R3 L5 L1 R4 R2 L4 L1 R3 R3 R1 L5 L2 R4 R4 R2 R1 R5 R5 L4 L1 R5 R3 R4 R5 R3 L1 L2 L4 R1 R4 R5
         L2 L3 R4 L4 R2 L2 L4 L2 R5 R1 R4 R3 R5 L4 L4 L5 L5 R3 R4 L1 L3 R2 L2 R1 L3 L5 R5 R5 R3 L4 L2 R4 R5 R1
         R4 L3
-      ])
+      ]
     end
 
     def test_shortest_distance
